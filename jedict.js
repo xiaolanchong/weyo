12fbead1b7ab932e9a5ef00c993aea360b24910d
xiaolanchong <heuretic@mail.ru>
2018-07-19 02:25:58 +0300

    Initial import

---
+++ jedict.js
@@ -0,0 +1,7 @@
+
+// word : [kanji, kana, definition]+
+const jedict = {
+	"第": [["第","だい","(n) program of a ceremony (programme)"]],
+	"いや": [["","だいい","(n) program of a ceremony (programme)"]],
+	"経過": [["","だいい","(n) program of a ceremony (programme)"]],
+}
